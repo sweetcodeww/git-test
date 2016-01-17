769f4d7c15e0fbfae7ff4b2e96b6c1e58c3c2de9
sweetcodeww <sweet.code.ww@gmail.com>
2016-01-17 21:23:52 +0700

    modifying about.php

--- about.php
+++ about.php
@@ -2,8 +2,8 @@
 print_r(
 	array(
 		'name' => 'Erwin Indrawan',
-		'email' => 'erwin.jnefer@gmail.com'
+		'email' => 'erwin.jnefer@gmail.com',
 		'address' => 'Jl.Amir Hamzah Gg.Actor No.1A Karang Sukun Mataram',
 		'phone' => '082359250040',
-		);
+		)
 	);
