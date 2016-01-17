7fe5c2f42786f0498c7e71763aae9e788fc4e192
Erwin Indrawan <erwin.jnefer@gmail.com>
2016-01-17 18:35:57 +0700

    adding about.php content

--- about.php
+++ about.php
@@ -3,6 +3,7 @@ print_r(
 	array(
 		'name' => 'Erwin Indrawan',
 		'email' => 'erwin.jnefer@gmail.com'
-		'address' => 'Jl.Amir Hamzah Gg.Actor No.1A Karang Sukun Mataram'
+		'address' => 'Jl.Amir Hamzah Gg.Actor No.1A Karang Sukun Mataram',
+		'phone' => '082359250040'
 		);
 	);
